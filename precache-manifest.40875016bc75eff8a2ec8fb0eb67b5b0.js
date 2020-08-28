6a2b0deffee902746b5170c9afd861a55c2f535b
Scott Caputo <scottvcaputo@gmail.com>
2020-08-27 20:08:41 -0400

    Updates

---
+++ precache-manifest.40875016bc75eff8a2ec8fb0eb67b5b0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758c91ba7f10e96fcdb4aa8c8fe5e05f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a5050aaf2b7c99d5f09c",
+    "url": "/Portfolio/static/css/2.66165e39.chunk.css"
+  },
+  {
+    "revision": "f237834899e4faf61a10",
+    "url": "/Portfolio/static/css/main.da18eaa0.chunk.css"
+  },
+  {
+    "revision": "a5050aaf2b7c99d5f09c",
+    "url": "/Portfolio/static/js/2.787dc1ca.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/Portfolio/static/js/2.787dc1ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f237834899e4faf61a10",
+    "url": "/Portfolio/static/js/main.f5b7933b.chunk.js"
+  },
+  {
+    "revision": "87bcb24a22a073d38476",
+    "url": "/Portfolio/static/js/runtime-main.d4446722.js"
+  },
+  {
+    "revision": "bfbe04b17234ecc50c629ac0b903790f",
+    "url": "/Portfolio/static/media/algomodal.bfbe04b1.png"
+  },
+  {
+    "revision": "d680b01d9b2a4e404b33e158ee3c7e54",
+    "url": "/Portfolio/static/media/mountaincarmodal.d680b01d.jpg"
+  },
+  {
+    "revision": "80fd901f7bf9d1ef3ece1b15afe886be",
+    "url": "/Portfolio/static/media/profilepic.80fd901f.jpg"
+  },
+  {
+    "revision": "de5fd01b3b6e6923d0062bd8f5742e8e",
+    "url": "/Portfolio/static/media/resume.de5fd01b.pdf"
+  },
+  {
+    "revision": "fe0a3d25eebde12f09342f038e6dbc97",
+    "url": "/Portfolio/static/media/styletransfer.fe0a3d25.PNG"
+  },
+  {
+    "revision": "f1b732a825f8bfe630d126a3a5197277",
+    "url": "/Portfolio/static/media/styletransfermodal.f1b732a8.jpg"
+  }
+]);
